74017e8e92619c4a53aa04a5ed112bd6676d159f
samandareo <smdeveloper04@gmail.com>
2024-07-29 01:15:26 +0500

    update

---
+++ app.py
@@ -0,0 +1,58 @@
+from flask import Flask, request, jsonify, render_template
+import json
+import db
+
+app = Flask(__name__)
+
+
+@app.route('/text')
+def text():
+    return render_template('change_text.html')
+
+@app.route('/import')
+def import_from_sheets():
+    if db.import_thread is not None and db.import_thread.is_alive():
+        return render_template('import.html', status="Status: Importing...")
+    return render_template('import.html')
+
+@app.route('/stop', methods=['POST'])
+def stop_import():
+    try:
+        db.stop_import()
+    except Exception as e:
+        print(e)
+
+    if db.import_thread is None:
+        return render_template('import.html', status="Imported is stopped")
+    else:
+        return render_template('import.html', status=f"Importing is not stopped...")
+    
+@app.route('/start', methods=['POST'])
+def start_import():
+    start_error = "Not yet started"
+    try:
+        db.start_import()
+    except Exception as e:
+        start_error = e
+
+    if db.import_thread is not None and db.import_thread.is_alive():
+        return render_template('import.html', status="Importing is started...")
+    else:
+        return render_template('import.html', status=f"Import is not started. Error: {start_error}")
+
+
+    
+@app.route('/change_text', methods=['POST'])
+def change_text():
+    user_text = request.form.get('user_text')
+    if user_text:
+        with open('extras/sms_text.json', 'w') as f:
+            json.dump({'text': user_text}, f)
+
+        return f'Text changed to: {user_text}'
+    else:
+        return 'No text provided.'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- db.py
+++ db.py
@@ -1,7 +1,10 @@
 import psycopg2 as pg
 from google.oauth2.service_account import Credentials
 from googleapiclient.discovery import build
-import time
+import threading
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.triggers.interval import IntervalTrigger
+import asyncio
 
 # Authenticate with Google Sheets
 creds = Credentials.from_service_account_file('extras/credentials.json')
@@ -9,7 +12,7 @@ service = build('sheets', 'v4', credentials=creds)
 
 spreadsheet_id = "13rgsyFiA7URNNE2XNCFvIqkMdQzoBJXURiQBtVsoWqY"
 
-global conn, cursor
+global conn, cursor, stop_event, import_thread, scheduler
 
 conn = pg.connect(
     host="dpg-cqitjr8gph6c738u4sp0-a.frankfurt-postgres.render.com",
@@ -20,76 +23,122 @@ conn = pg.connect(
 )
 
 cursor = conn.cursor()
+stop_event = threading.Event()
+import_thread = None
+scheduler = None
 
-
-def get_row_count(table_name):
+def get_row_count(tbname):
     try:
-        cursor.execute(f'SELECT COUNT(*) FROM {table_name}')
+        cursor.execute(f"SELECT count FROM rows_count WHERE name = '{tbname}'")
         return cursor.fetchone()[0]
     except Exception as e:
         print(e)
         return None
 
-def all_users(sheet_name, table_name):
-
-    row_count = get_row_count(table_name)
-    start_row = row_count + 2   
+async def all_users(sheet_name, table_name):
+    counter = get_row_count(table_name)
+    start_row = counter + 1
+    print(get_row_count(table_name))
+    print(f"Starting from row {start_row}")
     range_name = f'{sheet_name}!B{start_row}:D'
     result = service.spreadsheets().values().get(spreadsheetId=spreadsheet_id, range=range_name).execute()
     values = result.get('values', [])
-    
+
     if not values:
         print(f'No new data found in {sheet_name}.')
-        return None
+        return
     else:
         try:
             for row in values:
                 if len(row) >= 2:
                     cursor.execute(f"SELECT * FROM {table_name} WHERE phone_number = '{row[0]}' AND book_id = {row[2]}")
+                    counter += 1
                     result = cursor.fetchone()
-                    if result == None:
+                    if result is None:
                         print(f"Adding {row[1]} ({row[0]}) with book id {row[2]} to the database.")
                         cursor.execute(f"INSERT INTO {table_name} (name, phone_number, book_id) VALUES (%s, %s, %s)", (row[1], row[0], row[2]))
                         conn.commit()
                     else:
                         print(f'{row[1]} already exists in the database with this book id {row[2]}.')
-                    time.sleep(1)
+                await asyncio.sleep(2)
         except Exception as e:
             print(e)
-            return None
-        
-            
+        finally:
+            cursor.execute(f"UPDATE rows_count SET count = {counter} WHERE name = '{table_name}'")
+            conn.commit()
 
-def checked_users(sheet_name, table_name):
-    
-        row_count = get_row_count(table_name)
-        start_row = row_count + 2   
-        range_name = f'{sheet_name}!B{start_row}:c'
-        result = service.spreadsheets().values().get(spreadsheetId=spreadsheet_id, range=range_name).execute()
-        values = result.get('values', [])
+async def checked_users(sheet_name, table_name):
+    counter = get_row_count(table_name)
+    start_row = counter + 1
+    range_name = f'{sheet_name}!B{start_row}:C'
+    result = service.spreadsheets().values().get(spreadsheetId=spreadsheet_id, range=range_name).execute()
+    values = result.get('values', [])
+
+    if not values:
+        print(f'No new data found in {sheet_name}.')
+        return
+    else:
         try:
-            if not values:
-                print(f'No new data found in {sheet_name}.')
-                return None
-            else:
-                for row in values:
-                    print(f"{row[1]}'s number is {row[0]}")
-                    if len(row) >= 2:
-                        cursor.execute(f"SELECT * FROM {table_name} WHERE phone_number = '{row[0]}'")
-                        result = cursor.fetchone()
-                        if result == None:
-                            print(f"Adding {row[1]} ({row[0]}) to the database.")
-                            cursor.execute(f"INSERT INTO {table_name} (name, phone_number, book_id) VALUES (%s, %s, %s)", (row[1], row[0], row[2]))
-                            conn.commit()
-                        else:
-                            print(f'{row[1]} already exists in the database ')
-                        time.sleep(1)
+            for row in values:
+                counter += 1
+                print(f"{row[1]}'s number is {row[0]}")
+                if len(row) >= 2:
+                    cursor.execute(f"SELECT * FROM {table_name} WHERE phone_number = '{row[0]}'")
+                    result = cursor.fetchone()
+                    if result is None:
+                        print(f"Adding {row[1]} ({row[0]}) to the database.")
+                        cursor.execute(f"INSERT INTO {table_name} (name, phone_number) VALUES (%s, %s)", (row[1], row[0]))
+                        conn.commit()
+                    else:
+                        print(f'{row[1]} already exists in the database.')
+                await asyncio.sleep(2)
         except Exception as e:
             print(e)
-            return None
+        finally:
+            cursor.execute(f"UPDATE rows_count SET count = {counter} WHERE name = '{table_name}'")
+            conn.commit()
 
-def google_sheets_imports():
-    all_users('All', 'users')
-    checked_users('Chack', 'end_users')
+async def google_sheets_imports():
+    await all_users('All', 'users')
+    await checked_users('Chack', 'end_users')
 
-google_sheets_imports()
+async def import_data():
+    while not stop_event.is_set():
+        await google_sheets_imports()
+        await asyncio.sleep(60) 
+
+def run_import_data():
+    global loop
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    try:
+        loop.run_until_complete(import_data())
+    except asyncio.CancelledError:
+        pass
+    finally:
+        loop.close()
+
+def start_import():
+    global import_thread, scheduler
+    if import_thread is None or not import_thread.is_alive():
+        stop_event.clear()
+        import_thread = threading.Thread(target=run_import_data)
+        import_thread.start()
+
+def stop_import():
+    global import_thread, loop, stop_event
+
+    # Set the stop event to signal the thread to stop
+    stop_event.set()
+
+    if import_thread is not None:
+        # Stop the asyncio event loop
+        loop.call_soon_threadsafe(loop.stop)
+        
+        # Join the import thread to wait for it to terminate
+        import_thread.join()
+
+        print("Stopping loop...")
+        print("Import thread stopped.")
+        import_thread = None
+        loop = None
